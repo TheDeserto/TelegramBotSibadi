c12ad226c9a61d3f1d857c2852157cca02bcc2c2
Deserto <87060514+TheDeserto@users.noreply.github.com>
2022-11-09 14:31:13 +0600

    Revert "Исправлена ошибка: PTBUserWarning: Could not add signal handlers for the stop signals (<Signals.SIGINT: 2>, <Signals.SIGTERM: 15>, <Signals.SIGABRT: 22>) due to exception `NotImplementedError()`. Код приведён к стандарту PEP8 Упрощены if конструкции"
    This reverts commit c2b6298486d9ad0fa4ee9afeca0438d13127849a.

--- main.py
+++ main.py
@@ -9,49 +9,45 @@ from telegram.ext import (
     filters
 )
 
-
 async def start(update, context):
     buttons = [
-        ["Приёмная комиссия"],
-        ["Помощь", "Общежитие"]
+            ["Приёмная комиссия"],
+            ["Помощь", "Общежитие"]
     ]
 
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
-    if context.user_data.get("not first launch"):
+    if context.user_data.get("not first launch") == True:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Возвращение в начало.",
-            reply_markup=keyboard
+                chat_id=update.effective_chat.id,
+                text="Возвращение в начало.",
+                reply_markup=keyboard
         )
     else:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Привет, я помогу тебе с поступлением в СибАДИ.",
-            reply_markup=keyboard
+                chat_id=update.effective_chat.id,
+                text="Привет, я помогу тебе с поступлением в СибАДИ.",
+                reply_markup=keyboard
         )
 
     context.user_data["not first launch"] = True
 
-
 async def show_contacts(update, context):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=(
-            """Омск, проспект Мира, д. 5, каб. 1.139
+            chat_id=update.effective_chat.id,
+            text=(
+"""Омск, проспект Мира, д. 5, каб. 1.139
 Телефон +7(3812)65-99-88
 Email: priem_kom@sibadi.org
 Мессенджеры +79131499459
 """)
     )
 
-
 async def show_dormitory_info(update, context):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="<Вставить сюда информацию об общежитии>"
+            chat_id=update.effective_chat.id,
+            text="<Вставить сюда информацию об общежитии>"
     )
-
 
 async def show_admiration_committee_info(update, context):
     buttons = [
@@ -63,43 +59,42 @@ async def show_admiration_committee_info(update, context):
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Какую информацию показать?",
-        reply_markup=keyboard
+            chat_id=update.effective_chat.id,
+            text="Какую информацию показать?",
+            reply_markup=keyboard
     )
 
 
 async def unknown(update, context):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Извините, не могу ответить на этот вопрос."
+            chat_id=update.effective_chat.id,
+            text="Извините, не могу ответить на этот вопрос."
     )
-
 
 def main():
     with open('token.txt') as f:
-        token = f.readline()[:-1]  # remove \n
+        token = f.readline()[:-1] # remove \n
 
     application = ApplicationBuilder().token(token).build()
 
     application.add_handler(CommandHandler("start", start))
 
     application.add_handler(MessageHandler(
-        filters.Regex("^Помощь$"), show_contacts))
+            filters.Regex("^Помощь$"), show_contacts))
 
     application.add_handler(MessageHandler(
-        filters.Regex("^Общежитие$"), show_dormitory_info))
+            filters.Regex("^Общежитие$"), show_dormitory_info))
 
     application.add_handler(MessageHandler(
-        filters.Regex("^Приёмная комиссия$"), show_admiration_committee_info))
+            filters.Regex("^Приёмная комиссия$"), show_admiration_committee_info))
 
     application.add_handler(MessageHandler(
-        filters.Regex("^В начало$"), start))
+            filters.Regex("^В начало$"), start))
 
     application.add_handler(MessageHandler(filters.COMMAND | filters.TEXT, unknown))
 
-    application.run_polling(stop_signals=None)
-
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
+
