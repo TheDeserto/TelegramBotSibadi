7e26434093b208acf72ed2b0c2f03a91a3ada020
RHMN <nerzul843@gmail.com>
2022-11-23 13:59:19 +0600

    Дописаны коды

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from telegram.ext import (
 
 async def start(update, context):
     buttons = [
+        ["Образовательные программы"],
         ["Приёмная комиссия"],
         ["Помощь", "Общежитие"]
     ]
@@ -33,7 +34,46 @@ async def start(update, context):
 
     context.user_data["not first launch"] = True
 
+async def training_programs(update, context):
+    buttons = [
+        ["Бакалавриат"],
+        ["Специалитет"],
+        ["Магистратура"],
+        ["Аспирантура"],
+        ["В начало"]
+    ]
 
+    keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите программу подготовки:",
+        reply_markup=keyboard
+    )
+
+async def bachelor(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Программы подготовки Бакаравриат: https://sibadi.org/student/educational-programs/bachelor/"
+    )
+
+async def speciality(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Программы подготовки Специалитет: https://sibadi.org/student/educational-programs/speciality/"
+    )
+
+async def master(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Программы подготовки Магистратура: https://sibadi.org/student/educational-programs/master/"
+    )
+
+async def graduate(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Программы подготовки Аспирантура: https://sibadi.org/student/educational-programs/graduate/"
+    )
 async def show_contacts(update, context):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -49,7 +89,7 @@ Email: priem_kom@sibadi.org
 async def show_dormitory_info(update, context):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="<Вставить сюда информацию об общежитии>"
+        text="https://sibadi.org/entrant/hostel/?ysclid=las86yprkt78081357"
     )
 
 
@@ -67,6 +107,59 @@ async def show_admiration_committee_info(update, context):
         text="Какую информацию показать?",
         reply_markup=keyboard
     )
+
+
+async def document_feeding(update, context):
+    buttons = [
+        ["Подача документов онлайн"],
+        ["Подача почтой России", "Подача документов лично"],
+        ["В начало"]
+    ]
+    keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите способ подачи документов",
+        reply_markup=keyboard
+    )
+
+
+async def submission_online(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Инструкция для подачи онлайн: https://sibadi.org/abitur2021/lp/instruktsiyaonlayn/"
+    )
+
+
+async def send_mail(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Для подачи документов Почтой России отправьте копии "
+             "документов об образовании, документы удостоверяющие личность заказным письмом и"
+             "необходимые заявления по адресу 644080, Омская область, г. Омск, Пр. Мира 5, каб. 1.139"
+    )
+
+
+async def give_person(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Подать документы на поступление можно лично по адресу г. Омск, "
+             "пр. Мира 5, главный корпус, каб. 1.139, пн-птн с 9:00 до 16:00, сб.10:00 - 13.00"
+    )
+
+async def calendar(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="https://sibadi.org/abitur2021/lp/kalendarabiturienta/?ysclid=las9nfbavt528044682"
+    )
+
+async def required_documents(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="https://sibadi.org/entrant/commission/priemnaya-komissiya-2023/?ysclid=las8icfb3b36269298"
+    )
+
+
 ###
 #
 async def unknown(update, context):
@@ -91,7 +184,40 @@ def main():
         filters.Regex("^Общежитие$"), show_dormitory_info))
 
     application.add_handler(MessageHandler(
+        filters.Regex("^Необходимые документы$"), required_documents))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Образовательные программы$"), training_programs))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Бакалавриат$"), bachelor))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Специалитет$"), speciality))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Магистратура$"), master))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Аспирантура$"), graduate))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Подача$"), document_feeding))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Подача документов онлайн$"), submission_online))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Подача почтой России$"), send_mail))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Подача документов лично$"), give_person))
+
+    application.add_handler(MessageHandler(
         filters.Regex("^Приёмная комиссия$"), show_admiration_committee_info))
+
+    application.add_handler(MessageHandler(
+        filters.Regex("^Сроки приёма$"), calendar))
 
     application.add_handler(MessageHandler(
         filters.Regex("^В начало$"), start))
